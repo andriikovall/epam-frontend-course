ec33f370e4b7b23f1dea162a5670778a6ef8c4bd
ziovio <anderkoval3@gmail.com>
2020-03-02 17:15:45 +0200

    task2 done and cganged eslint config

--- hw3/.eslintrc.js
+++ hw3/.eslintrc.js
@@ -1,17 +1,44 @@
 module.exports = {
-  extends: "airbnb-base",
-  env: {
-    browser: true
-  },
-  rules: {
-    'no-console': 0,
-    'no-alert': 0,
-    'no-plusplus': 0,
-    'no-use-before-define': 0,
-    'arrow-parens': 0,
-    'new-cap': 0,
-    'func-names': 0,
-    'no-underscore-dangle': 0,
-    'radix': 0
-  }
+	"extends": "eslint:recommended",
+	"env": {
+		"browser": true,
+	},
+	"rules": {
+		"no-console": "off",
+		"no-extra-parens": ["error", "all"],
+		"array-callback-return": "error",
+		"curly": "error",
+		"default-case": "warn",
+		"eqeqeq": ["error", "always"],
+		"guard-for-in": "warn",
+		"no-caller": "error",
+		"no-empty-function": "error",
+		"no-eval": "error",
+		"no-extra-bind": "error",
+		"no-floating-decimal": "error",
+		"no-lone-blocks": "error",
+		"no-multi-spaces": "error",
+		"no-new": "warn",
+		"no-return-assign": "error",
+		"no-self-compare": "error",
+		"no-useless-call": "error",
+		"no-undef-init": "error",
+		"block-spacing": "error",
+		"brace-style": "error",
+		"comma-dangle": ["error", "never"],
+		"func-call-spacing": ["error", "never"],
+		"max-len": ["error", { "code": 120, "ignoreComments": true }],
+		"new-cap": ["error", { "newIsCap": true }],
+		"new-parens": "error",
+		"no-nested-ternary": "error",
+		"no-unneeded-ternary": "error",
+		"quotes": ["warn", "single", { "avoidEscape": true , "allowTemplateLiterals": true }],
+		"arrow-spacing": ["error", { "before": true, "after": true }],
+		"no-useless-computed-key": "error",
+		"no-useless-constructor": "error",
+		"no-prototype-builtins": "off",
+		"no-var": "warn",
+		"no-unused-vars": ["error", { "vars": "local" }],
+		"no-magic-numbers": ["warn", { "ignore": [0, 1] }]
+	}
 }
---
+++ hw3/js/task4.js
@@ -0,0 +1,27 @@
+const arr = [
+  { value: 100, type: 'USD' },
+  { value: 215, type: 'EUR' },
+  { value: 7, type: 'EUR' },
+  { value: 99, type: 'USD' },
+  { value: 354, type: 'USD' },
+  { value: 12, type: 'EUR' },
+  { value: 77, type: 'USD' },
+];
+
+
+function task4() {
+  console.log(task4_1());
+  console.log(task4_2());
+}
+
+function task4_1() {
+  return arr.filter(item => item.value < 100 && item.type === 'USD')
+            .map(item => item.value)
+            .reduce((accum, curr) => accum + curr);
+}
+
+function task4_2() {
+  return arr.filter(item => item.type === 'EUR')
+            .map(item => ({ ...item, value: item.value * 2 }));
+}
+
