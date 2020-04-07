0bfffc519553aa29073a5eb2f830c39db3996646
ziovio <anderkoval3@gmail.com>
2020-04-07 19:36:04 +0300

    moved dirs

--- coursework/cinema/src/app/app.module.ts
+++ coursework/cinema/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
-import { MainComponent } from './components/layout/main/main.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
