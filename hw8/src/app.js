821aeb479844dd67e3d6d0c371875221c8787dfc
ziovio <anderkoval3@gmail.com>
2020-05-04 17:51:12 +0300

    app init

---
+++ hw8/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app.css';
+
+const App = (props) => {
+    return (
+        <div>
+            <h1>My REACT APP</h1>
+        </div>
+    )
+}
+
+
+export default App;
