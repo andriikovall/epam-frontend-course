7b5f5a4ae082cac35009644b11ffc70cf3e19b53
ziovio <anderkoval3@gmail.com>
2020-05-05 13:37:07 +0300

    created app with script after efforts

--- hw8/src/app.js
+++ hw8/src/app.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
+import Home from './components/pages/home';
+import Login from './components/pages/login';
 
 import './app.css';
 
 const App = (props) => {
     return (
-        <div>
-            <h1>My REACT APP</h1>
-        </div>
+        <>
+            <Link to="/home">Home</Link>
+            <Link to="/login">Login</Link>
+            <Switch>
+                <Route path="/home" component={Home} />
+                <Route path="/login" component={Login} />
+                <Redirect to="/login" />
+            </Switch>
+        </>
     )
 }
 
--- hw8/webpack.config.js
+++ hw8/webpack.config.js
@@ -43,7 +43,6 @@ module.exports = {
           ], [])
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 8080
     }
