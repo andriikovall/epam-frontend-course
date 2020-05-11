a22023d749f0b39994f1c5125c84d31178d03475
Andrey Koval <andrey.koval@tecwi.ch>
2020-05-11 21:35:54 +0300

    made loader

--- coursework/cinema/src/app/components/loader/loader.component.ts
+++ coursework/cinema/src/app/components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
+
+  @Input() heigth: string = '1em';
 
   constructor() { }
 
