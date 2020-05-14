bbe314c35838655234b0034e9d27c4546dab0a2e
Andrey Koval <andrey.koval@tecwi.ch>
2020-05-14 22:17:49 +0300

    improved http interseptor

--- coursework/cinema/src/app/services/htppinterseptor.service.ts
+++ coursework/cinema/src/app/services/htppinterseptor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, } from '@angular/common/http';
-import { Observable, of, throwError } from "rxjs";
-import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from "rxjs";
+import { catchError } from 'rxjs/operators';
 import { BaseService } from './base.service';
 
 @Injectable({
@@ -12,6 +12,7 @@ export class HTPPInterseptorService implements HttpInterceptor {
   constructor(private baseService: BaseService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.baseService.networkError.next(false);
     return next.handle(req).pipe(
       catchError(err => {
         this.baseService.networkError.next(true);
